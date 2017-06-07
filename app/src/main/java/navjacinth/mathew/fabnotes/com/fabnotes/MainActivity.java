a923e14bc582aadec4002e14f2f3de09ee948df8
Navjacinth Mathew <jacinth9@gmail.com>
2017-06-07 21:38:35 +0530

    Added Project

---
+++ app/src/main/java/navjacinth/mathew/fabnotes/com/fabnotes/MainActivity.java
@@ -0,0 +1,13 @@
+package navjacinth.mathew.fabnotes.com.fabnotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
