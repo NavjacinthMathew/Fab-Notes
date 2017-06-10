a6ea85275dd794bdb5ee2cd17c1114f2a584b8db
Navjacinth Mathew <jacinth9@gmail.com>
2017-06-10 07:24:41 +0530

    Updated layout, styles and colors

--- app/src/main/java/navjacinth/mathew/fabnotes/com/fabnotes/activity/HomeActivity.java
+++ app/src/main/java/navjacinth/mathew/fabnotes/com/fabnotes/activity/HomeActivity.java
@@ -2,9 +2,8 @@ package navjacinth.mathew.fabnotes.com.fabnotes.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import navjacinth.mathew.fabnotes.com.fabnotes.R;
 
@@ -25,7 +25,7 @@ public class HomeActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_home);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
