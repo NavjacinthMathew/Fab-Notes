4c5a9c0e51a30c6109b846c40035e17550156d07
Navjacinth Mathew <jacinth9@gmail.com>
2017-06-11 19:28:24 +0530

    -Updated note model

--- app/src/main/java/navjacinth/mathew/fabnotes/com/fabnotes/model/NoteModel.java
+++ app/src/main/java/navjacinth/mathew/fabnotes/com/fabnotes/model/NoteModel.java
@@ -13,4 +13,34 @@ public class NoteModel extends RealmObject {
     private String title;
     private String content;
     private Date timeStamp;
+
+    public NoteModel(String title, String content, Date timeStamp) {
+        this.title = title;
+        this.content = content;
+        this.timeStamp = timeStamp;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
+    }
 }
