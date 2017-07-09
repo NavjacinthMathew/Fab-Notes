a03bafc3874fa7ab38712ae13e1f13c94c74e6dc
Navjacinth Mathew <jacinth9@gmail.com>
2017-07-09 21:13:43 +0530

    -Code clean up

--- app/src/main/java/navjacinth/mathew/fabnotes/com/fabnotes/view/fragment/AllNoteFragment.java
+++ app/src/main/java/navjacinth/mathew/fabnotes/com/fabnotes/view/fragment/AllNoteFragment.java
@@ -2,8 +2,6 @@ package navjacinth.mathew.fabnotes.com.fabnotes.view.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
